e027d85ba65aaf1ea50d5a376da169bb4a5d4f9f
nepalese-noob <85428811+nepalese-noob@users.noreply.github.com>
2021-11-10 06:39:00 +0545

    Add files via upload

---
+++ desktop.sh
@@ -0,0 +1,94 @@
+read -p "do you want to start desktop? y/n: " dt
+if [ "$dt" = "y" ]; then
+nh kex
+fi
+read -p "do you want to copy something? y/n: " cpy
+if [ "$cpy" = "y" ]; then
+	echo "1. file"
+	echo "2. folder"
+	read -p "choose category: " category
+	if [ "$category" = "1" ]; then
+		echo "you choosed 1"
+		sleep 1
+		clear
+		echo "1. Home to desktop"
+		echo "2. Home 2 sdcard"
+		echo "3. sdcard to home"
+		echo "4. sdcard to desktop"
+		echo "5. desktop to home"
+		echo "6. desktop to sdcard"
+		read wh
+		if [ "$wh" = "1" ]; then
+			cd ~ && ls
+			read -p "enter file name: " file
+			cp $file /data/data/com.termux/files/home/kali-arm64/home/kali/Desktop
+
+		elif [ "$wh" = "2" ]; then
+			cd ~ && ls
+		read -p "enter file name: " file
+			cp $file /sdcard
+		elif [ "$wh" = "3" ]; then
+			cd /sdcard && ls
+			read -p "enter file name: " file
+			cp $file $HOME
+		elif [ "$wh" = "4" ]; then
+			cd /sdcard
+			ls
+			read -p "enter file name: " file
+			cp $file /data/data/com.termux/files/home/kali-arm64/home/kali/Desktop
+		elif [ "$wh" = "5" ]; then
+			cd /data/data/com.termux/files/home/kali-arm64/home/kali/Desktop
+			ls
+			read -p "enter file name: " file
+			cp $file $HOME
+		elif [ "$wh" = "6" ]; then
+			cd /data/data/com.termux/files/home/kali-arm64/home/kali/Desktop
+                        ls
+                        read -p "enter file name: " file
+			cp $file /sdcard
+		else
+			echo ""
+		fi
+	elif [ "$category" = "2" ]; then
+                echo "you choosed 1"
+                sleep 1
+                clear
+                echo "1. Home to desktop"
+                echo "2. Home 2 sdcard"
+                echo "3. sdcard to home"
+                echo "4. sdcard to desktop"
+                echo "5. desktop to home"
+                echo "6. desktop to sdcard"
+                read wh
+                if [ "$wh" = "1" ]; then
+                        cd ~ && ls                                                                                      read -p "enter file name: " file
+                        cp -r $file /data/data/com.termux/files/home/kali-arm64/home/kali/Desktop
+
+                elif [ "$wh" = "2" ]; then
+                        cd ~ && ls
+                read -p "enter file name: " file
+                        cp -r $file /sdcard
+                elif [ "$wh" = "3" ]; then
+                        cd /sdcard && ls
+                        read -p "enter file name: " file
+                        cp -r $file $HOME
+                elif [ "$wh" = "4" ]; then
+                        cd /sdcard
+                        ls
+                        read -p "enter file name: " file
+                        cp -r $file /data/data/com.termux/files/home/kali-arm64/home/kali/Desktop
+                elif [ "$wh" = "5" ]; then
+                        cd /data/data/com.termux/files/home/kali-arm64/home/kali/Desktop
+                        ls
+                        read -p "enter file name: " file
+                        cp -r $file $HOME
+                elif [ "$wh" = "6" ]; then
+                        cd /data/data/com.termux/files/home/kali-arm64/home/kali/Desktop
+                        ls
+                        read -p "enter file name: " file
+                        cp -r $file /sdcard
+                else
+                        echo ""
+		fi
+	fi
+fi
